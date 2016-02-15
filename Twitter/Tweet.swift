3bbea25840e2a31837ec22b3d39d6e10dc5ed093
THANH PHAM <tpham44@mail.ccsf.edu>
2016-02-14 18:10:39 -0800

    fix

---
+++ Twitter/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  Twitter
+//
+//  Created by JP on 2/14/16.
+//  Copyright © 2016 tpham44. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
---
+++ Twitter/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by JP on 2/14/16.
+//  Copyright © 2016 tpham44. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
